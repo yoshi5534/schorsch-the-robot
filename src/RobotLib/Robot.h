423cff11e908bf713bc2f10d5f14069d5f90f340
schorsch <schorsch@localhost>
2011-08-19 22:31:37 +0200

    added goHome() function that sets a hard coded home position and moves to it

--- src/RobotLib/Robot.h
+++ src/RobotLib/Robot.h
@@ -79,6 +79,12 @@ class Robot
         {
             _robotPort->sendCommand("TI " + dataToString(static_cast< uint16 >(timeInMiliseconds / 10) ));
         }
+
+        void goHome()
+        {
+           _robotPort->sendCommand("PD 1,+207.43,-14.00,+338.59,+36.88,+178.84,R,A,O");
+           _robotPort->sendCommand("MO 1");
+        }
 };
 
 #endif // ROBOT_H

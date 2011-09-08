0c8f170ab59f7a87c3d5ce2d054b19d777eb30b0
schorsch <schorsch@localhost>
2011-09-08 22:41:07 +0200

    implemented method

--- src/RobotLib/Text.h
+++ src/RobotLib/Text.h
@@ -3,13 +3,37 @@
 
 #include <list>
 #include "Vector.h"
+#include "Matrix.h"
+#include "Robot.h"
+#include <string>
 
 class Text
 {
     public:
-      std::string processString(std::string inputString)
+      static void processString(Robot* robot, std::string stringToParse, Matrix targetCoordinateSystem)
       {
-	
+	  Vector spaceVector(1.5, 0, 0);
+	  
+	  for(size_t i = 0; i < stringToParse.length(); i++)
+	  {
+	      std::list<Vector> sourceCoordinateMoveList;
+	      switch(stringToParse[i])
+	      {
+		case 'a': sourceCoordinateMoveList = a(); break;
+		case 'b': sourceCoordinateMoveList = b(); break;
+		case 'c': sourceCoordinateMoveList = c(); break;		
+		default: sourceCoordinateMoveList = square(); break;
+	      }
+	      
+	      for(std::list<Vector>::iterator it = sourceCoordinateMoveList.begin(); it != sourceCoordinateMoveList.end(); it++)
+	      {
+		  Vector sourceVector = *it;
+		  Vector targetVector = targetCoordinateSystem * sourceVector;
+		  
+		  robot->moveTo(targetVector, 0, 0);
+		  robot->moveTo(targetCoordinateSystem * spaceVector, 0,0);
+	      }	      
+	  }
       }
 
 //     std::list<Vector> a()
@@ -21,7 +45,7 @@ class Text
 //     
 private:
     
-    std::list<Vector> a()
+    static std::list<Vector> a()
     {
 	 std::list<Vector> list;
 	 list.push_back( Vector( 1.0, 0.0, 1.0) );
@@ -35,7 +59,7 @@ private:
     }
     
     
-    std::list<Vector> b()
+    static std::list<Vector> b()
     {
 	 std::list<Vector> list;
 	 list.push_back( Vector( 0.0, 2.0, 1.0) );	
@@ -48,7 +72,7 @@ private:
 	 return list;
     }
 
-    std::list<Vector> c()
+    static std::list<Vector> c()
     {
 	 std::list<Vector> list;
 	 list.push_back( Vector( 1.0, 1.0, 1.0) );	
@@ -59,5 +83,17 @@ private:
 	 list.push_back( Vector( 1.0, 0.0, 1.0) );	
 	 return list;
     }
-
+     
+    static std::list<Vector> square()
+    {
+	 std::list<Vector> list;
+	 list.push_back( Vector( 1.0, 0.0, 1.0) );
+	 list.push_back( Vector( 1.0, 0.0, 0.0) );
+	 list.push_back( Vector( 0.0, 0.0, 0.0) );
+	 list.push_back( Vector( 0.0, 1.0, 0.0) );
+	 list.push_back( Vector( 1.0, 1.0, 0.0) );
+	 list.push_back( Vector( 1.0, 1.0, 1.0) );
+	 return list;
+    }
+};
 #endif // TEXT_H

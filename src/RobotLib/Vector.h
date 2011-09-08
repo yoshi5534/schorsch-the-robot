2198475fd5631878573b84f101c5d361c1758461
schorsch <schorsch@localhost>
2011-09-08 22:40:54 +0200

    renamed some varibles

--- src/RobotLib/Vector.h
+++ src/RobotLib/Vector.h
@@ -3,6 +3,7 @@
 
 
 #include "Types.h"
+#include <math.h>
 
 
 //Vector in mm
@@ -21,21 +22,38 @@ class Vector
 	}
 	
 	
-    Vector( float64 x, float64 y, float64 z)
+	Vector( float64 x, float64 y, float64 z)
 	{
 	  this->x = x; 
 	  this->y = y; 
 	  this->z = z; 
 	}
 	
-	Vector operator+( const Vector& other )
+	Vector operator+( const Vector& otherVector ) const
 	{
-		return Vector( this->x + other.x, this->y + other.y, this->z + other.z );
+		return Vector( this->x + otherVector.x, this->y + otherVector.y, this->z + otherVector.z );
 	}
 
-	Vector operator-( const Vector& other )
+	Vector operator-( const Vector& otherVector ) const
 	{
-		return Vector( this->x - other.x, this->y - other.y, this->z - other.z );
+		return Vector( this->x - otherVector.x, this->y - otherVector.y, this->z - otherVector.z );
+	}
+	
+	Vector crossProduct(const Vector& otherVector) const
+	{
+		return Vector( 	(this->y * otherVector.z) - (this->z * otherVector.y),
+				(this->z * otherVector.x) - (this->x * otherVector.z),
+				(this->x * otherVector.y) - (this->y * otherVector.x)
+		             );
+	}
+	
+	Vector getNormalized() const
+	{
+	    float64 length;
+	    
+	    length = sqrt(this->x*this->x + this->y * this->y + this->z * this->z);
+	    
+	    return Vector(this->x / length, this->y/ length, this->z/length);	  
 	}
 };
 

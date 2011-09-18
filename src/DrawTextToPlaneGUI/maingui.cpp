6a3720c3c98a44129d17b065e028f90afb5850ef
schorsch <schorsch@localhost>
2011-09-18 18:53:34 +0200

    made GUI much more cooler

--- src/DrawTextToPlaneGUI/maingui.cpp
+++ src/DrawTextToPlaneGUI/maingui.cpp
@@ -1,6 +1,12 @@
 #include "maingui.h"
 #include "ui_maingui.h"
 
+#include "../RobotLib/Matrix.h"
+#include "../RobotLib/Vector.h"
+#include "../RobotLib/Text.h"
+#include "../RobotLib/PlaneToCoordinateSystem.h"
+#include "../RobotLib/Where.h"
+
 mainGUI::mainGUI(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::mainGUI)
@@ -15,10 +21,19 @@ mainGUI::mainGUI(QWidget *parent) :
     ui->leYVectorX->setValidator(new QDoubleValidator(this));
     ui->leYVectorY->setValidator(new QDoubleValidator(this));
     ui->leYVectorZ->setValidator(new QDoubleValidator(this));
+    
+    this->robot = new Robot("/dev/ttyS0", 750000);
+    this->robot->getPort()->setLiveCommandMode(false);
 }
 
 mainGUI::~mainGUI()
 {
+    if(this->robot != NULL)
+    { 
+      delete this->robot;
+      this->robot = NULL;      
+    }
+
     delete ui;
 }
 
@@ -33,3 +48,52 @@ void mainGUI::changeEvent(QEvent *e)
         break;
     }
 }
+
+void mainGUI::defineYVector()
+{
+    Where where(robot);
+    ui->leYVectorX->setText(QString::number(where.x));
+    ui->leYVectorY->setText(QString::number(where.y));
+    ui->leYVectorZ->setText(QString::number(where.z));
+}
+
+void mainGUI::defineXVector()
+{
+    Where where(robot);
+    ui->leXVectorX->setText(QString::number(where.x));
+    ui->leXVectorY->setText(QString::number(where.y));
+    ui->leXVectorZ->setText(QString::number(where.z)); 
+}
+
+void mainGUI::defineBaseVector()
+{
+    Where where(robot);
+    ui->leBaseVectorX->setText(QString::number(where.x));
+    ui->leBaseVectorY->setText(QString::number(where.y));
+    ui->leBaseVectorZ->setText(QString::number(where.z));  
+}
+    
+void mainGUI::writeText()
+{  
+    Vector yPoint	( ui->leYVectorX->text().toDouble(), 	ui->leYVectorY->text().toDouble(), 	ui->leYVectorZ->text().toDouble());
+    Vector basePoint	( ui->leBaseVectorX->text().toDouble(), ui->leBaseVectorY->text().toDouble(), 	ui->leBaseVectorZ->text().toDouble());
+    Vector xPoint	( ui->leXVectorX->text().toDouble(), 	ui->leXVectorY->text().toDouble(), 	ui->leXVectorZ->text().toDouble());
+    
+    Matrix coordinateSystem = PlaneToCoodinateSystem::toCoordinateSystem(xPoint-basePoint, yPoint-basePoint);
+    Vector target = coordinateSystem * Vector(1,0,0);
+     
+    float64 AngleA = 80;
+    float64 AngleB = 80;
+    
+    Text::writeTextWithWordWrap( 
+				this->robot, 
+				std::string(ui->txtEditTextToWrite->toPlainText().toStdString()), 
+				coordinateSystem, 
+				yPoint, 
+				20.0,
+				10
+				);     
+    
+    this->robot->getPort()->executeQuedCommands();
+}
+
--- src/DrawTextToPlaneGUI/maingui.h
+++ src/DrawTextToPlaneGUI/maingui.h
@@ -2,6 +2,7 @@
 #define MAINGUI_H
 
 #include <QMainWindow>
+#include "../RobotLib/Robot.h"
 
 namespace Ui {
     class mainGUI;
@@ -18,10 +19,13 @@ protected:
 
 private:
     Ui::mainGUI *ui;
+    Robot *robot;
 
 public slots:
-    void writeText();
-
+    void writeText();   
+    void defineYVector();
+    void defineXVector();
+    void defineBaseVector(); 
 
 };
 

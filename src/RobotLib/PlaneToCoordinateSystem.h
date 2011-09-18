c5cb8effa3a7b92ea55fdcf4d6680e461b37d9ea
schorsch <schorsch@localhost>
2011-09-18 12:38:57 +0200

    we now use an simpliefied vector sheme to calcualte our coordinate system (horay to sunday morning power)

--- src/RobotLib/PlaneToCoordinateSystem.h
+++ src/RobotLib/PlaneToCoordinateSystem.h
@@ -8,24 +8,21 @@ class PlaneToCoodinateSystem
   
 public:
   
-    static Matrix toCoordinateSystem( Vector baseVector, Vector firstVector, Vector secondVector )
+    static Matrix toCoordinateSystem( Vector xVector, Vector yVector)
     {
-      Vector normalizedFirstTempVector  =  (firstVector  - baseVector).getNormalized();
-      Vector normalizedSecondTempVector =  (secondVector - baseVector).getNormalized();
-      Vector crossProduct = (normalizedSecondTempVector.crossProduct( normalizedFirstTempVector)).getNormalized();
-      //Vector crossProduct = (normalizedFirstTempVector.crossProduct( normalizedSecondTempVector)).getNormalized();
-      Vector correctedSecondVector = crossProduct.crossProduct(normalizedFirstTempVector).getNormalized();
-      //Vector correctedSecondVector = normalizedFirstTempVector.crossProduct(crossProduct).getNormalized();
-      normalizedFirstTempVector = normalizedFirstTempVector * -1;
-      correctedSecondVector = correctedSecondVector * -1;
-      Matrix matrix( normalizedFirstTempVector.x, normalizedFirstTempVector.y, normalizedFirstTempVector.z,
-		     correctedSecondVector.x, correctedSecondVector.y, correctedSecondVector.z,
-		     crossProduct.x, crossProduct.y, crossProduct.z );
+      Vector normalizedXVector =  xVector.getNormalized();
+      Vector normalizedYVector =  yVector.getNormalized();
+      Vector normalizedZVector = (normalizedXVector.crossProduct( normalizedYVector)).getNormalized();
+      
+      //we are not sure if xVector and yVector are orthogonal to each other so we define an new helper vector which is orthogonal
+      Vector correctedXVector = normalizedYVector.crossProduct(normalizedZVector).getNormalized();
+      
+      Matrix matrix( correctedXVector .x, correctedXVector .y, correctedXVector .z,
+		     normalizedYVector.x, normalizedYVector.y, normalizedYVector.z,
+		     normalizedZVector.x, normalizedZVector.y, normalizedZVector.z);
       
       return matrix;      
-    }
-    
-    
+    }    
 };
 
 #endif //__PLANETOCOORDINAESYSTEM_H__20110908_

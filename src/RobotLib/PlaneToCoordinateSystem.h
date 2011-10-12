70cca1179e54284f47aa86b56e9b11ec54fcfaa9
schorsch <schorsch@localhost>
2011-10-12 20:40:13 +0200

    possibility of axis invertion added

--- src/RobotLib/PlaneToCoordinateSystem.h
+++ src/RobotLib/PlaneToCoordinateSystem.h
@@ -1,21 +1,32 @@
 #ifndef __PLANETOCOORDINAESYSTEM_H__20110908_
 #define __PLANETOCOORDINAESYSTEM_H__20110908_
 
+#include <stdexcept>
 #include "Vector.h"
 
 class PlaneToCoodinateSystem
 {
   
 public:
-  
-    static Matrix toCoordinateSystem( Vector xVector, Vector yVector)
+    static Matrix toCoordinateSystem( Vector xVector, Vector yVector, bool invertX = false, bool invertY = false, bool invertZ = false)
     {
       Vector normalizedXVector =  xVector.getNormalized();
       Vector normalizedYVector =  yVector.getNormalized();
-      Vector normalizedZVector = (normalizedXVector.crossProduct( normalizedYVector)).getNormalized();
+      Vector normalizedZVector;
+      Vector correctedXVector;
       
+      normalizedZVector = (normalizedXVector.crossProduct( normalizedYVector)).getNormalized();
       //we are not sure if xVector and yVector are orthogonal to each other so we define an new helper vector which is orthogonal
-      Vector correctedXVector = normalizedYVector.crossProduct(normalizedZVector).getNormalized();
+      correctedXVector = normalizedYVector.crossProduct(normalizedZVector).getNormalized();
+
+      if(invertX)
+	correctedXVector *= -1;
+      
+      if(invertY)
+	normalizedYVector *= -1;
+      
+      if(invertZ)
+	normalizedZVector *= -1;
       
       Matrix matrix( correctedXVector .x, correctedXVector .y, correctedXVector .z,
 		     normalizedYVector.x, normalizedYVector.y, normalizedYVector.z,

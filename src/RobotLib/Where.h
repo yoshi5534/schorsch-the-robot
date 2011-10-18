eb4eeac02d533b4019f904671334466769ba9194
schorsch <schorsch@localhost>
2011-10-18 19:46:59 +0200

    moved where to proper location and implementet an method to acces the home possition

--- src/RobotLib/Where.h
+++ src/RobotLib/Where.h
@@ -1,11 +1,7 @@
 #ifndef WHERE_H
 #define WHERE_H
 
-#include <list>
-#include "Vector.h"
-#include "Robot.h"
 #include "HelperMethods.h"
-#include <string>
 
 class Where
 {
@@ -14,45 +10,6 @@ class Where
       float64 y;
       float64 z;
       float64 a;
-      float64 b;
-      
-      Where(Robot* robot)
-      {
-	 std::string whereResult = robot->getPort()->sendAndReceive("WH");
-	 size_t lastPosition = 0;
-
-	 for(uint8 index = 0; index < 5; index++)
-	 {
-	      size_t currentPosition = whereResult.find_first_of(',',lastPosition);
-	      size_t countOfCharacters = currentPosition - lastPosition;
-	      std::string result = whereResult.substr(lastPosition, countOfCharacters);
-	      
-	      lastPosition = currentPosition + 1;
-	//+145.55,+594.87,+776.09,+123.76,+79.03,R,A,O
-	      
-	      if(index == 0)
-	      {
-		  x = stringToData< float64 >( result );
-	      }
-	      else if(index == 1)
-	      {
-		  y = stringToData< float64 >( result );
-	      }
-	      else if(index == 2)
-	      {
-		  z = stringToData< float64 >( result );
-	      }
-	      else if(index == 3)
-	      {
-		  a = stringToData< float64 >( result );
-	      }
-	      else if(index == 4)
-	      {
-		  b = stringToData< float64 >( result );
-	      }
-	 }
-      }
-      
-
+      float64 b;     
 };
 #endif // WHERE_H

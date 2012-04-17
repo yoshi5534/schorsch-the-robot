e8300bfc9dbe2d2d3177bf7d59d7dc97b6274be1
schorsch <schorsch@localhost>
2012-04-17 20:21:15 +0200

    first working GUI draft, that can control the presentation

--- src/Control2012/ImpressAutomation.cpp
+++ src/Control2012/ImpressAutomation.cpp
@@ -11,8 +11,8 @@
 // before executing the program you have to run when no libreoffice is running:
 // soffice --accept="socket,host=localhost,port=2083;urp;StarOffice.ServiceManager"
 
-
-// call console -env:URE_MORE_TYPES="file:////usr/lib/libreoffice/basis3.4/program/offapi.rdb" "file:////home/oswald/projekt/control/control2011.ppt" "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager"
+//comand line parameters for executable
+// -env:URE_MORE_TYPES="file:////usr/lib/libreoffice/basis3.4/program/offapi.rdb" "file:////home/oswald/projekt/control/control2011.ppt" "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager"
 
 // 
 
@@ -34,12 +34,12 @@
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/bridge/XUnoUrlResolver.hpp>
-#include <com/sun/star/frame/XComponentLoader.hpp>
 #include <com/sun/star/lang/XMultiComponentFactory.hpp>
 #include <com/sun/star/registry/XSimpleRegistry.hpp>
+#include <com/sun/star/presentation/XPresentationSupplier.hpp>
+#include <com/sun/star/presentation/XPresentation2.hpp>
 
-#include <string.h>
-
+#include <string>
 
 
 //#include <stdio.h>
@@ -49,14 +49,12 @@
 // added
 //#include "/usr/include/libreoffice/com/sun/star/frame/XComponentLoader.hpp"
 
-
-
-
+#include <stdexcept>
 #include "ImpressAutomation.h"
 
-ImpressAutomation::ImpressAutomation()
+ImpressAutomation::ImpressAutomation (com::sun::star::uno::Reference<com::sun::star::frame::XComponentLoader> componentLoader)
 {
-  
+  xComponentLoader = componentLoader;
 }
 
 ImpressAutomation::~ImpressAutomation()
@@ -64,125 +62,79 @@ ImpressAutomation::~ImpressAutomation()
   
 }
 
-int ImpressAutomation::connect()
+void ImpressAutomation::loadPresentation(std::string fileName)
 {
+  #define UNX
+  #define SAL_UNX
+  #define CPPU_ENV
+  
   using namespace com::sun::star::uno;
   using namespace com::sun::star::lang;
   using namespace com::sun::star::beans;
   using namespace com::sun::star::bridge;
   using namespace com::sun::star::frame;
   using namespace com::sun::star::registry;
-  
+
   using ::rtl::OUString;
   using ::rtl::OUStringToOString;
   
   
-  OUString sConnectionString(RTL_CONSTASCII_USTRINGPARAM("uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager"));
-  
-  sal_Int32 nCount = (sal_Int32)rtl_getAppCommandArgCount();
-  
-  //if (nCount < 1)
-  //{
-    //   printf("using: DocumentLoader -env:URE_MORE_TYPES=<office_types_rdb_url> <file_url> [<uno_connection_url>]\n\n"
-  //           "example: DocumentLoader -env:URE_MORE_TYPES=\"file:///.../basis-link/program/offapi.rdb\" \"file:///e:/temp/test.odt\" \"uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager\"\n");
-  //(   exit(1);
-  //}
-  //if (nCount == 2)
-  //{
-    //   rtl_getAppCommandArg(1, &sConnectionString.pData);
-  //}
-  
-  
-  Reference< XComponentContext > xComponentContext(::cppu::defaultBootstrap_InitialComponentContext());
-  
-  /* Gets the service manager instance to be used (or null). This method has
-   *       been added for convenience, because the service manager is a often used
-   *       object.
-   */
-  Reference< XMultiComponentFactory > xMultiComponentFactoryClient(
-    xComponentContext->getServiceManager() );
-  
-  /* Creates an instance of a component which supports the services specified
-   *       by the factory.
-   */
-  Reference< XInterface > xInterface =
-  xMultiComponentFactoryClient->createInstanceWithContext( 
-  OUString(RTL_CONSTASCII_USTRINGPARAM("com.sun.star.bridge.UnoUrlResolver")),
-				       xComponentContext );
-  
-  Reference< XUnoUrlResolver > resolver( xInterface, UNO_QUERY );
-  
-  // Resolves the component context from the office, on the uno URL given by argv[1].
-  try
-  {    
-    xInterface = Reference< XInterface >( 
-    resolver->resolve( sConnectionString ), UNO_QUERY );
-  }
-  catch ( Exception& e )
-  {
-    printf("Error: cannot establish a connection using '%s':\n       %s\n",
-	   OUStringToOString(sConnectionString, RTL_TEXTENCODING_ASCII_US).getStr(),
-	   OUStringToOString(e.Message, RTL_TEXTENCODING_ASCII_US).getStr());
-  //exit(1);    
-  usleep(1000);
-  }
-  
-  
-  // gets the server component context as property of the office component factory
-  Reference< XPropertySet > xPropSet( xInterface, UNO_QUERY );
-  xPropSet->getPropertyValue( OUString(RTL_CONSTASCII_USTRINGPARAM("DefaultContext")) ) >>= xComponentContext;
-  
-  // gets the service manager from the office
-  Reference< XMultiComponentFactory > xMultiComponentFactoryServer(
-    xComponentContext->getServiceManager() );
-  
-  /* Creates an instance of a component which supports the services specified
-   *       by the factory. Important: using the office component context.
-   */
-  Reference < XComponentLoader > xComponentLoader(
-    xMultiComponentFactoryServer->createInstanceWithContext( 
-    OUString( RTL_CONSTASCII_USTRINGPARAM("com.sun.star.frame.Desktop" ) ),
-					  xComponentContext ), UNO_QUERY );
-    
-    /* Loads a component specified by an URL into the specified new or existing
-     *       frame.
-     */
-    OUString sWorkingDir, sDocPathUrl;
+      /* Loads a component specified by an URL into the specified new or existing
+       frame.
+    */
+    //OUString sAbsoluteDocUrl, sWorkingDir, sDocPathUrl, sArgDocUrl;
     //rtl_getAppCommandArg(0, &sArgDocUrl.pData);
     
-    //rtl_uString_newFromAscii( &sArgDocUrl.pData,"file:///home/oswald/projekt/schorsch-the-robot/src/Control2012/test.odt");
-    OUString sAbsoluteDocUrl(RTL_CONSTASCII_USTRINGPARAM("file:////home/oswald/projekt/schorsch-the-robot/src/Control2012/test.odt"));
+   //OUString sAbsoluteDocUrl(RTL_CONSTASCII_USTRINGPARAM(""));
+    //OUString sWorkingDir(RTL_CONSTASCII_USTRINGPARAM(""));
+   // OUString sDocPathUrl(RTL_CONSTASCII_USTRINGPARAM(""));
+    OUString sArgDocUrl(RTL_CONSTASCII_USTRINGPARAM("file:////home/oswald/projekt/control/control2011.ppt"));
     
-    
-    //     osl_getProcessWorkingDir(&sWorkingDir.pData);
-    //     osl::FileBase::getFileURLFromSystemPath( sArgDocUrl, sDocPathUrl);
-    //     osl::FileBase::getAbsoluteFileURL( sWorkingDir, sDocPathUrl, sAbsoluteDocUrl);
-    try{
-      Reference< XComponent > xComponent = xComponentLoader->loadComponentFromURL(
-	sAbsoluteDocUrl, OUString( RTL_CONSTASCII_USTRINGPARAM("_blank") ), 0,
-							       Sequence < ::com::sun::star::beans::PropertyValue >() );
-    }   
-    catch ( Exception& e )
-    {
-      printf("Error: cannot establish a connection using '%s':\n       %s\n",
+// -env:URE_MORE_TYPES="file:////usr/lib/libreoffice/basis3.4/program/offapi.rdb" "file:////home/oswald/projekt/control/control2011.ppt" "uno:socket,host=localhost,port=2083;urp;StarOffice.ServiceManager"
+
+   // osl_getProcessWorkingDir(&sWorkingDir.pData);
+   //osl::FileBase::getFileURLFromSystemPath( sArgDocUrl, sDocPathUrl);
+   // osl::FileBase::getAbsoluteFileURL( sWorkingDir, sDocPathUrl, sAbsoluteDocUrl);
+/*    
+     printf("connection '%s':\n      url %s\n",
 	     OUStringToOString(sConnectionString, RTL_TEXTENCODING_ASCII_US).getStr(),
-	     OUStringToOString(e.Message, RTL_TEXTENCODING_ASCII_US).getStr());
-      //exit(1);    
-      usleep(1000);
-    }
+	     OUStringToOString(sArgDocUrl, RTL_TEXTENCODING_ASCII_US).getStr());
+    */
+    Reference< XComponent > xComponent = xComponentLoader->loadComponentFromURL(
+        sArgDocUrl, OUString( RTL_CONSTASCII_USTRINGPARAM("_blank") ), 0,
+        Sequence < ::com::sun::star::beans::PropertyValue >() );
     
-    
-    // dispose the local service manager
-    Reference< XComponent >::query( xMultiComponentFactoryClient )->dispose();
+
+    Reference< ::com::sun::star::presentation::XPresentationSupplier > showsupplier(xComponent, UNO_QUERY); 
+    Reference< ::com::sun::star::presentation::XPresentation > show = showsupplier->getPresentation(); 
+//     Reference< XPropertySet > mPres_Props(show, UNO_QUERY); 
+//     sal_Int16 startpage = 0; 
+//     mPres_Props->setPropertyValue(createStr("FirstPage"), makeAny(startpage) ); 
+//   show->start(); 
+    Reference< ::com::sun::star::presentation::XPresentation2 > show2(show, UNO_QUERY);
+    presentation = show2;
 }
 
 
+void ImpressAutomation::startPresentation()
+{
+    presentation->start();
+}
 
+void ImpressAutomation::stopPresentation()
+{
+    presentation->end();
+}
 
+void ImpressAutomation::nextSlide()
+{
+    presentation->getController()->gotoNextSlide();
+}
 
-
-
-
+void ImpressAutomation::previousSlide()
+{
+    presentation->getController()->gotoPreviousSlide();
+}
 
 
 
--- src/Control2012/maingui.h
+++ src/Control2012/maingui.h
@@ -6,26 +6,34 @@
 #include <QTime>
 #include "../RobotLib/Robot.h"
 
-namespace Ui {
+#include "ImpressAutomation.h"
+
+namespace Ui 
+{
 class mainGUI;
 }
 
 class mainGUI : public QMainWindow {
     Q_OBJECT
 public:
-    mainGUI(QWidget *parent = 0);
+    mainGUI(com::sun::star::uno::Reference<com::sun::star::frame::XComponentLoader> createdXComponentLoader, QWidget *parent = 0);
     ~mainGUI();
 
 private:
     Ui::mainGUI *ui;
     Robot *robot;
-    
+    ImpressAutomation impressAutomation;
+        
     bool robotIsAtHomePossition(float64 allowedDelta);
 
 public slots:
     void abortTransmission();
     void uploadProgram();
-    void yesClicked();
+    void loadPresentation();
+    void startPresentation();
+    void stopPresentation();
+    void nextSlide();
+    void previousSlide();
 
     
 };

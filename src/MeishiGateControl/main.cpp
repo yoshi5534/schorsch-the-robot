465644c326b8653dd4a17ea8e861abfcf4b4a326
schorsch <schorsch@localhost>
2013-04-17 22:00:29 +0200

    different stuff mostly meyshi gate related;

--- src/MeishiGateControl/main.cpp
+++ src/MeishiGateControl/main.cpp
@@ -1,10 +1,12 @@
 
 
 #include "MeishiGateControl.h"
-
+#include <iostream>
 
 int main(int argc, char** argv)
 {
-  
+  MeishiGateControl meishiGateControl("/dev/ttyACM0");
+  meishiGateControl.uploadProgram();
+  meishiGateControl.run();
   return 0; 
 }
